27e0fea0ae091f112c103c14c2adcdc573540ea7
squaredcanine <quintenstekie@gmail.com>
2018-11-07 10:36:21 +0100

    Highscore.json does not have to be present, a new file will be created if it does not exist

--- src/Main.hs
+++ src/Main.hs
@@ -9,13 +9,14 @@ import System.Random
 import GenericTypes
 import Data.Aeson
 
+import System.Directory
 import Graphics.Gloss.Interface.IO.Game
 import qualified Data.ByteString.Lazy as B
 import Data.Either
 
 main :: IO ()
 main = do  ran <- getStdGen
-           highscore <- getJsonFile
+           doesExist <- doesFileExist jsonFile
            putStrLn "###################################################################"
            putStrLn "INSTRUCTIONS:"
            putStrLn "Use arrow keys to move. Press 'r' to restart from the beginning."
@@ -23,19 +24,29 @@ main = do  ran <- getStdGen
            putStrLn "###################################################################"
            putStrLn "Enter your name and press ENTER to start."
            nameGiven <- getLine
-           case highscore of 
-            Left err  ->  playIO (InWindow "Metal Dog 3" windowSizeInt (0, 0)) -- Or FullScreen
-                          black                     -- Background color
-                          30                        -- Frames per second
-                          (initialState ran nameGiven [])     -- Initial state
-                          view                      -- View function
-                          input                     -- Event function
-                          step                      -- Step function
-            Right ps  ->  playIO (InWindow "Metal Dog 3" windowSizeInt (0, 0)) -- Or FullScreen
-                          black                     -- Background color
-                          30                        -- Frames per second
-                          (initialState ran nameGiven ps)     -- Initial state
-                          view                      -- View function
-                          input                     -- Event function
-                          step                      -- Step function
-                 
+           if doesExist
+            then do highscore <- getJsonFile
+                    case highscore of 
+                      Left err  ->  playIO (InWindow "Metal Dog 3" windowSizeInt (0, 0)) -- Or FullScreen
+                                black                     -- Background color
+                                30                        -- Frames per second
+                                (initialState ran nameGiven [])     -- Initial state
+                                view                      -- View function
+                                input                     -- Event function
+                                step                      -- Step function
+                      Right ps  ->  playIO (InWindow "Metal Dog 3" windowSizeInt (0, 0)) -- Or FullScreen
+                                black                     -- Background color
+                                30                        -- Frames per second
+                                (initialState ran nameGiven ps)     -- Initial state
+                                view                      -- View function
+                                input                     -- Event function
+                                step                      -- Step function
+            else do playIO (InWindow "Metal Dog 3" windowSizeInt (0, 0)) -- Or FullScreen
+                                black                     -- Background color
+                                30                        -- Frames per second
+                                (initialState ran nameGiven [])     -- Initial state
+                                view                      -- View function
+                                input                     -- Event function
+                                step
+           
+           
